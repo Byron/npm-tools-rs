288fc0f2258986cf2e7d744fa81937705ed8d4df
Sebastian Thiel <byronimo@gmail.com>
2016-08-20 15:53:58 +0200

    fix(error): cause for *any* error
    As a result of [this conversation][gh-issue], dereffing
    the box ourselves seems to be the optimal solution.
    
    [gh-issue]: https://github.com/tailhook/quick-error/pull/28

--- src/dedup.rs
+++ src/dedup.rs
@@ -56,8 +56,7 @@ quick_error!{
             description("The visitor produced an error when changing")
             display("An error occurred: {}", err)
             context(p: VisitorContext<'a>, err: Box<StdError>) -> (p.0.to_path_buf(), err)
-            // TODO: Put in when merged: https://github.com/tailhook/quick-error/pull/28
-            // cause(err)
+            cause(&**err)
         }
     }
 }
